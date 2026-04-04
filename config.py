361bb1afab937f00a44552ae35080d7b0ba696fa
Brian <brianmartins997232779@gmail.com>
2026-04-04 12:52:46 -0300

    fix: adiciona suporte para banco free sqlite

--- config.py
+++ config.py
@@ -9,9 +9,8 @@ class Config:
     # A chave secreta que você gerou
     SECRET_KEY = os.getenv('SECRET_KEY', 'chave_secreta_fallback_para_dev')
     
-    # Configuração da URI de conexão com o MySQL
-    # O PyMySQL é o driver que faz a conexão
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    # Configuração da URI de conexão. Tenta buscar uma URL pronta inteira primeiro, se não achar, monta a do MySQL
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}")
     
     # Define como False para não ocupar recursos desnecessariamente em projetos novos
     SQLALCHEMY_TRACK_MODIFICATIONS = False
